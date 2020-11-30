fc35c99aa544de83cfa259af4ea4ebc7000b33d5
Erick Seigi Koda <ekoda@uolinc.com>
2020-11-30 17:48:20 -0300

    Adicionado EhCache

--- src/main/java/com/kazale/pontointeligente/api/services/impl/LancamentoServiceImpl.java
+++ src/main/java/com/kazale/pontointeligente/api/services/impl/LancamentoServiceImpl.java
@@ -5,6 +5,8 @@ import java.util.Optional;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
@@ -28,13 +30,17 @@ public class LancamentoServiceImpl implements LancamentoService{
 			return this.lancamentoRepository.findByFuncionarioId(funcionarioId, pageRequest);
 		}
 
-		@Override
+		//@Override		
+		//utilizando o ehcache, lancamentoPorId esta configurado no arquivo (/PontoInteligente/src/main/resources/ehcache.xml)
+		@Cacheable("lancamentoPorId") //ira cachear o resultado da buscarPorId()
 		public Optional<Lancamento> buscarPorId(Long id) {
 			log.info("Buscando lancamentos pelo ID {}", id);
 			return this.lancamentoRepository.findById(id);
 		}
 
-		@Override
+		//@Override
+		//@CachePut mantem o cache (lancamentoPorId) atualizado, caso tenha alguma alteracao no dado o cache tbm sera atualizado
+		@CachePut("lancamentoPorId")
 		public Lancamento persistir(Lancamento lancamento) {
 			log.info("Persistindo o lancamento: {}", lancamento);
 			return this.lancamentoRepository.save(lancamento);
